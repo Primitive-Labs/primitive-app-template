38e51eef11b3460ee79420bf1694b3364a731635
Carl-Primitive <carl@primitivelabs.com>
2025-12-02 10:36:45 -0800

    Fixes for logging; added getting started page for skeletongate and logger.

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { getAppConfig, getSingleDocumentConfig } from "@/config/appConfig";
 import { getJsBaoConfig } from "@/config/envConfig";
 import { templateNavigationConfig } from "@/config/navigationConfig";
-import { createPrimitiveApp } from "primitive-app";
+import { createPrimitiveApp, type LogLevel } from "primitive-app";
 import App from "./App.vue";
 import TestApp from "./TestApp.vue";
 import router from "./router/routes";
 import "./style.css";
+
+const envLogLevel =
+  (import.meta.env.VITE_LOG_LEVEL?.toLowerCase().trim() as LogLevel | undefined) ??
+  "debug";
 
 void createPrimitiveApp({
   mainComponent: App,
@@ -16,6 +20,7 @@ void createPrimitiveApp({
   navigationConfig: templateNavigationConfig,
   getSingleDocumentConfig,
   loginUrl: "/login",
+  logLevel: envLogLevel,
 });
 
 if ("serviceWorker" in navigator) {
