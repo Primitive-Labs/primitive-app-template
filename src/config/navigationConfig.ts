bd70a3eeb4a283299d11a4dbd443f01d075aae0a
Carl-Primitive <carl@primitivelabs.com>
2025-12-04 19:14:56 -0800

    Update navigation config and mobile nav options.

--- src/config/navigationConfig.ts
+++ src/config/navigationConfig.ts
@@ -1,16 +1,25 @@
 import type { NavigationConfig } from "primitive-app";
+import { NavigationOverflowMode } from "primitive-app";
 
 export const templateNavigationConfig: NavigationConfig = {
-  home: {
-    key: "home",
-    navTitle: "Home",
-    navGroup: "main",
-    routeName: "home",
+  navOptions: {
+    overflowMode: NavigationOverflowMode.Auto,
+    maxVisibleTabs: 5,
+    mobileNavEnabled: true,
+    mobileBackEnabled: true,
   },
-  logout: {
-    key: "logout",
-    navTitle: "Log out",
-    navGroup: "user-menu",
-    routeName: "logout",
+  navItems: {
+    home: {
+      key: "home",
+      navTitle: "Home",
+      navGroup: "main",
+      routeName: "home",
+    },
+    logout: {
+      key: "logout",
+      navTitle: "Log out",
+      navGroup: "user-menu",
+      routeName: "logout",
+    },
   },
 };
